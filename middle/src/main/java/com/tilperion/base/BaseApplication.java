687a4013a841ece23234f3c87155e09b275e3361
Andrei Merenkov <amerenkov@exadel.com>
2018-04-20 16:41:52 +0300

    feature change

--- middle/src/main/java/com/tilperion/base/BaseApplication.java
+++ middle/src/main/java/com/tilperion/base/BaseApplication.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 public class BaseApplication {
 
 	public static void main(String[] args) {
-		//middle message
+		//feature change
 		SpringApplication.run(BaseApplication.class, args);
 	}
 }
